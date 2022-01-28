92dc09504fcdcd5e23fe6361aca0d5fd78625d9e
Gerard <gfblazkez@hotmail.com>
2022-01-27 20:09:37 -0500

    Minor changes in naming variables

--- plot1.R
+++ plot1.R
@@ -1,4 +1,8 @@
-## Check if zipfile exists in directory
+## Install & load tidyverse package
+install.packages("tidyverse")
+library(tidyverse)
+
+## Check if zip file exists in directory
 zipfile<- "exdata_data_household_power_consumption.zip"
 if (file.exists(!zipfile)){
   download.file("https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip",
@@ -18,13 +22,13 @@ df<-read_delim("household_power_consumption.txt",delim=";",
                                 col_double(),col_double(),col_double(),col_double(),col_double()))
 
 ## extract data from 2007-02-01 & 2007-02-02
-df_03<-df5%>% filter(Date=="2007-02-01"| Date=="2007-02-02")
+df1<-df%>% filter(Date=="2007-02-01"| Date=="2007-02-02")
 
 ## Plot1
 ## open device 
 png(filename = "plot1.png")
 ## plot graphic
-hist(df_03$Global_active_power, main="Global Active Power",
+hist(df1$Global_active_power, main="Global Active Power",
      xlab = "Global Active Power (kilowatts)",col = "red")
 ## close device
 dev.off()
